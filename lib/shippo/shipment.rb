5a8dc44e90b15d9047741f94f93e76b78f367640
Laurynas Butkus <laurynas.butkus@gmail.com>
2014-11-02 11:21:23 +0200

    Add lowest shipment rate helper

--- lib/shippo/shipment.rb
+++ lib/shippo/shipment.rb
@@ -2,6 +2,7 @@ module Shippo
   class Shipment < Resource
     include Shippo::Operations::List
     include Shippo::Operations::Create
+
     def rates(currency=nil, params={})
       if !currency.nil?
         response = Shippo.request(:get, "#{url}/rates/#{currency}/", params)
@@ -9,6 +10,12 @@ module Shippo
         response = Shippo.request(:get, "#{url}/rates/", params)
       end
       Shippo::Rate.construct_from(response[:results])
+    end
+
+    def lowest_rate(provider = nil, currency = nil, params = {})
+      records = rates(currency, params)
+      records = records.select { |r| r.provider == provider } if provider
+      records.min_by { |r| r.amount.to_f }
     end
   end
 end
