8ee8c17169bb298fa6eb9fc1abb1f3e538ca7af9
David Tien <davidtien@Davids-MacBook-Pro.local>
2017-01-23 10:15:19 -0800

    remove breaking test

--- spec/shippo/api/model/address_spec.rb
+++ spec/shippo/api/model/address_spec.rb
@@ -5,11 +5,3 @@ RSpec.describe Shippo::Address do
     it { is_expected.to  eql('/addresses') }
   end
 end
-
-RSpec.describe Shippo::Address do
-  context 'validate' do
-  	v = Shippo::Address.validate('0b1fe132b4d14f5eaee4fd8d480452bc')
-  	puts v.object.id
-  	expect(v.object.id).to eql("c0b8bb8104bd4b67adb4bd57dd886f77")
-  end
-end
