a14a04aadc7269ab3b2d5e3636b243a456ac648f
Vinh Vu <kroomey@gmail.com>
2017-02-14 14:45:40 -0800

    Add mock test for Batch creation

---
+++ spec/dummy_helper.rb
@@ -0,0 +1,146 @@
+DUMMY_BATCH = {
+  "default_carrier_account": "bb980a0dcd2047328024bc3dcad56682",
+  "default_servicelevel_token": "usps_priority",
+  "label_filetype": "ZPLII",
+  "metadata": "BATCH #170",
+  "batch_shipments": [
+    {
+      "shipment": {
+        "object_purpose": "PURCHASE",
+        "address_from": {
+          "object_purpose": "PURCHASE",
+          "name": "Simon Says",
+          "company": "",
+          "street1": "965 Mission St",
+          "street2": "Ste 201",
+          "city": "San Francisco",
+          "state": "CA",
+          "zip": "94103",
+          "country": "US",
+          "phone": "4151234567",
+          "email": "hippo@goshippo.com"
+        },
+        "address_to": {
+          "object_purpose": "PURCHASE",
+          "name": "Who Says",
+          "company": "",
+          "street1": "400 Simon St",
+          "street_no": "",
+          "street2": "Apt 123",
+          "city": "San Francisco",
+          "state": "CA",
+          "zip": "94102",
+          "country": "US",
+          "phone": "4151234567",
+          "email": "hippo@goshippo.com",
+          "metadata": ""
+        },
+        "parcel": {
+          "length": "5",
+          "width": "5",
+          "height": "5",
+          "distance_unit": "in",
+          "weight": "2",
+          "mass_unit": "oz",
+          "template": "",
+          "metadata": "Customer ID 123456"
+        },
+        "reference_1": "REF1 - Simon",
+        "reference_2": "REF2 - Says",
+        "metadata": ""
+      }
+    },
+    {
+      "shipment": {
+        "object_purpose": "PURCHASE",
+        "address_from": {
+          "object_purpose": "PURCHASE",
+          "name": "Simon Says",
+          "company": "",
+          "street1": "965 Mission St",
+          "street2": "Ste 201",
+          "city": "San Francisco",
+          "state": "CA",
+          "zip": "94103",
+          "country": "US",
+          "phone": "4151234567",
+          "email": "shippo@goshippo.com"
+        },
+        "address_to": {
+          "object_purpose": "PURCHASE",
+          "name": "Who Says",
+          "company": "",
+          "street1": "400 Simon St",
+          "street_no": "",
+          "street2": "Apt 123",
+          "city": "San Francisco",
+          "state": "CA",
+          "zip": "94102",
+          "country": "US",
+          "phone": "4151234567",
+          "email": "hippo@goshippo.com",
+          "metadata": ""
+        },
+        "parcel": {
+          "length": "5",
+          "width": "5",
+          "height": "5",
+          "distance_unit": "in",
+          "weight": "2",
+          "mass_unit": "oz",
+          "template": "",
+          "metadata": "Customer ID 123456"
+        },
+        "reference_1": "REF1 - Simon",
+        "reference_2": "REF2 - Says",
+        "metadata": ""
+      }
+    },
+    {
+      "shipment": {
+        "object_purpose": "PURCHASE",
+        "address_from": {
+          "object_purpose": "PURCHASE",
+          "name": "Simon Says",
+          "company": "",
+          "street1": "965 Mission St",
+          "street2": "Ste 201",
+          "city": "San Francisco",
+          "state": "CA",
+          "zip": "94103",
+          "country": "US",
+          "phone": "4151234567",
+          "email": "hippo@goshippo.com"
+        },
+        "address_to": {
+          "object_purpose": "PURCHASE",
+          "name": "Who Says",
+          "company": "",
+          "street1": "400 Simon St",
+          "street_no": "",
+          "street2": "Apt 123",
+          "city": "San Francisco",
+          "state": "CA",
+          "zip": "94102",
+          "country": "US",
+          "phone": "4151234567",
+          "email": "hippo@goshippo.com",
+          "metadata": ""
+        },
+        "parcel": {
+          "length": "5",
+          "width": "5",
+          "height": "5",
+          "distance_unit": "in",
+          "weight": "2",
+          "mass_unit": "oz",
+          "template": "",
+          "metadata": "Customer ID 123456"
+        },
+        "reference_1": "REF1 - Simon",
+        "reference_2": "REF2 - Says",
+        "metadata": ""
+      }
+    }
+  ]
+}
