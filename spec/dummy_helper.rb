43deb438aff6f79a7516715f262a67373bf6ec10
Vinh Vu <kroomey@gmail.com>
2017-02-14 23:23:00 -0800

    Add dummy Shipment object

--- spec/dummy_helper.rb
+++ spec/dummy_helper.rb
@@ -144,3 +144,44 @@ DUMMY_BATCH = {
     }
   ]
 }
+
+DUMMY_SHIPMENT = {
+    "object_purpose": "PURCHASE",
+    "address_to": {
+        "object_purpose": "PURCHASE",
+        "name": "First Last",
+        "company": "",
+        "street1": "965 Mission St",
+        "street2": "#123",
+        "city": "San Francisco",
+        "state": "CA",
+        "zip": "94103",
+        "country": "US",
+        "phone": "4151234567",
+        "email": "hippo@goshippo.com",
+    },
+    "address_from": {
+        "object_purpose": "PURCHASE",
+        "name": "Amazon Fulfillment Services",
+        "company": "Returns Departments",
+        "street1": "400 Simon St",
+        "street2": "123",
+        "city": "San Francisco",
+        "state": "CA",
+        "zip": "94102",
+        "country": "US",
+        "phone": "4151234567",
+        "email": "hippo@goshippo.com"
+    },
+    "parcel": {
+        "length": "10",
+        "width": "15",
+        "height": "10",
+        "distance_unit": "in",
+        "weight": "15",
+        "mass_unit": "oz",
+        "metadata": ""
+    },
+    "reference_1": "hippo reference",
+    "reference_2": "another hippo/reference",
+}
