ca65d946ad8c1a39e1eacde38ede723aa2980d3e
Vinh Vu <kroomey@gmail.com>
2017-02-14 23:26:24 -0800

    Add method to retrieve a VALID Batch

--- spec/shippo/api/model/batch_spec.rb
+++ spec/shippo/api/model/batch_spec.rb
@@ -4,8 +4,25 @@ require 'spec_helper'
 DEFAULT_CARRIER_ACCOUNT = 'bb980a0dcd2047328024bc3dcad56682'
 DEFAULT_SERVICELEVEL_TOKEN = 'usps_priority'
 
+# Retries up to 10 times to retrieve a valid
+# Batch (which takes time to become 'VALID' from
+# 'VALIDATING' after initially being created).
+def retrieve_valid_batch(id)
+  retries = 10
+  retrieve = nil
+  until retries == 0 do
+    sleep 1
+    retrieve = Shippo::Batch::get(id)
+    break if retrieve[:object_status] == 'VALID'
+    retries -= 1
+  end
+  STDERR.puts 'Unable to retrieve VALID Batch object' unless retrieve
+  retrieve
+end
+
 RSpec.describe 'Shippo::API::Batch' do
   let(:dummy_batch) { DUMMY_BATCH }
+  let(:dummy_shipment) { DUMMY_SHIPMENT }
 
   describe '#create' do
     it 'should properly create and return a Batch object' do
