053c839c0ee7d729b259c5d697b032c1f996c491
Vinh Vu <vinh@goshippo.com>
2017-03-29 16:40:24 -0700

    Move test tokens/carriers variables to spec_helper.rb

--- spec/shippo/api/model/batch_spec.rb
+++ spec/shippo/api/model/batch_spec.rb
@@ -1,11 +1,6 @@
 require 'dummy_helper'
 require 'spec_helper'
 
-DEFAULT_CARRIER_ACCOUNT = '903074429eab4954b72df8a70defdfe3'
-DEFAULT_SERVICELEVEL_TOKEN = 'usps_priority'
-
-Shippo::API.token = 'shippo_test_09e74f332aa839940e6c241bb008157c19428339'
-
 # Retries up to 10 times to retrieve a valid
 # Batch (which takes time to become 'VALID' from
 # 'VALIDATING' after initially being created).
